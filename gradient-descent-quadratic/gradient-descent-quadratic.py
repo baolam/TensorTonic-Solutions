653feda2d71ce61baaef062f5918cf7c3a1176e5
Nguyễn Đức Bảo Lâm <63187255+baolam@users.noreply.github.com>
2026-03-07 15:34:53 +0700

    Update solution for gradient-descent-quadratic

--- gradient-descent-quadratic/gradient-descent-quadratic.py
+++ gradient-descent-quadratic/gradient-descent-quadratic.py
@@ -3,11 +3,9 @@ def gradient_descent_quadratic(a, b, c, x0, lr, steps):
     Return final x after 'steps' iterations.
     """
     # Write code here
-    def f_prime(x):
-        return 2 * a * x + b
-
+    # 2ax + b
     x = x0
-    for _ in range(steps):
-        x = x - lr * f_prime(x)
-
+    for i in range(steps):
+        x = x - lr * (2 * a * x + b)
     return x
+    
