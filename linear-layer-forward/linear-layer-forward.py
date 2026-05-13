85aa04d407ff243e1edf0a6c0647c280e8384237
Nguyễn Đức Bảo Lâm <63187255+baolam@users.noreply.github.com>
2026-05-13 08:09:21 +0700

    Update solution for linear-layer-forward

--- linear-layer-forward/linear-layer-forward.py
+++ linear-layer-forward/linear-layer-forward.py
@@ -5,10 +5,7 @@ def linear_layer_forward(X, W, b):
     Compute the forward pass of a linear (fully connected) layer.
     """
     # Write code here
-    X = np.asarray(X)
-    W = np.asarray(W)
-    b = np.asarray(b)
-
-    o = X @ W + b
-    o = o.tolist()
-    return o
+    X = np.array(X)
+    W = np.array(W)
+    b = np.array(b)
+    return (X @ W + b).tolist()
