4912a896df2b7a54edb0f76d7ecd9c19aab9fed1
Nguyễn Đức Bảo Lâm <63187255+baolam@users.noreply.github.com>
2026-05-22 14:54:29 +0700

    Add solution for geometric-pmf-mean

---
+++ geometric-pmf-mean/geometric-pmf-mean.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def geometric_pmf_mean(k, p):
+    """
+    Compute Geometric PMF and Mean.
+    """
+    # Write code here
+    k = np.array(k)
+    o = (1 - p) ** (k - 1) * p
+    return (o, 1/p)
