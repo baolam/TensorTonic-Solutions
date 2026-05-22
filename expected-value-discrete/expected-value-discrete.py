a1717728b2439a65a84a15d364a4056806e84dcc
Nguyễn Đức Bảo Lâm <63187255+baolam@users.noreply.github.com>
2026-05-22 14:58:11 +0700

    Update solution for expected-value-discrete

--- expected-value-discrete/expected-value-discrete.py
+++ expected-value-discrete/expected-value-discrete.py
@@ -5,8 +5,9 @@ def expected_value_discrete(x, p):
     Returns: float expected value
     """
     # Write code here
-    x = np.asarray(x)
-    p = np.asarray(p)
-    if p.sum() != 1:
-        raise ValueError("ValueError")
-    return (x * p).sum()
+    x = np.array(x)
+    p = np.array(p)
+    if not np.allclose(np.sum(p), 1, atol=1e-6):
+        raise ValueError("")
+    o = (x * p).sum()
+    return o
