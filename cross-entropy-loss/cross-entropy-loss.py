1aafb1566462ea50db69f8fbda48fa85625ceb1e
Nguyễn Đức Bảo Lâm <63187255+baolam@users.noreply.github.com>
2026-07-03 22:56:56 +0700

    Solve cross-entropy-loss

--- cross-entropy-loss/cross-entropy-loss.py
+++ cross-entropy-loss/cross-entropy-loss.py
@@ -6,10 +6,15 @@ def cross_entropy_loss(y_true, y_pred):
     Compute average cross-entropy loss for multi-class classification.
     """
     # Write code here
-    N = len(y_true)
-    s = 0
+    # N = len(y_true)
+    # s = 0
 
-    for l, logits in zip(y_true, y_pred):
-        s += math.log(logits[l])
+    # for l, logits in zip(y_true, y_pred):
+    #     s += math.log(logits[l])
     
-    return -1 / N * s
+    # return -1 / N * s
+    y_true = np.array(y_true)
+    y_pred = np.array(y_pred)
+    row_indices = np.arange(len(y_true))
+    logits = - np.log(y_pred[row_indices, y_true]).mean()
+    return logits
