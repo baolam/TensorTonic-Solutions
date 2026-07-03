35c40ce64da83a3df8b5e3e9ddc43b7f5da19fcc
Nguyễn Đức Bảo Lâm <63187255+baolam@users.noreply.github.com>
2026-07-03 22:40:06 +0700

    Solve leaky-relu

--- leaky-relu/leaky-relu.py
+++ leaky-relu/leaky-relu.py
@@ -5,5 +5,5 @@ def leaky_relu(x, alpha=0.01):
     Vectorized Leaky ReLU implementation.
     """
     # Write code here
-    x = np.asarray(x)
-    return np.where(x > 0, x, x * alpha)
+    x = np.array(x)
+    return np.where(x >= 0, x, alpha * x)
